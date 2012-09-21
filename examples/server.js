9c3aff565ed7e8d7997fe6cae38b7d0c10aeceb6
Brian Link <cpsubrian@gmail.com>
2012-09-21 15:50:20 -0700

    Updates to Cantina 2.x API.

--- examples/server.js
+++ examples/server.js
@@ -1,53 +1,47 @@
-var cantina = require('cantina');
+var app = require('cantina');
 
-var authHelpers = {
-  name: 'authHelpers',
-  init: function(app, done) {
-    app.serializeUser = function(user, done) {
-      console.log(user);
-      done(null, user);
-    };
-    app.deserializeUser = function(obj, done) {
-      done(null, obj);
-    };
-    app.verifyFacebook = function(token, tokenSecret, profile, done) {
-      done(null, profile);
-    };
-    done();
-  }
-};
+app.load(function(err) {
+  if (err) return console.error(err);
 
-var plugins = [
-  'http',
-  'middleware',
-  'cantina-redis',
-  'cantina-session',
-  authHelpers,
-  'cantina-auth',
-  '../'
-];
-
-var conf = {
-  'http': { port: 3000 },
-  'auth-facebook': {
-    clientID: '388250807064',
-    clientSecret: '640d4fa0ff8ed12e95facddfe3b83651',
-    callbackURL: 'http://localhost:3000/auth/facebook/callback',
-    authURL: '/login',
-    scope: 'email'
-  }
-};
-
-cantina.createApp(plugins, conf, function(err, app) {
-  if (err) return console.log(err);
-
-  app.middleware.get('/', function index(req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
-    if (req.isAuthenticated()) {
-      res.end('Welcome, <a href="https://facebook.com/' + req.user.username + '">' + req.user.displayName + '</a>!');
+  app.conf.add({
+    'http': { port: 3000 },
+    'auth-facebook': {
+      clientID: '388250807064',
+      clientSecret: '640d4fa0ff8ed12e95facddfe3b83651',
+      callbackURL: 'http://localhost:3000/auth/facebook/callback',
+      authURL: '/login',
+      scope: 'email'
     }
-    else {
-      res.end('<a href="/login">click here to login via Facebook</a>');
-    }
+  });
+
+  require(app.plugins.http);
+  require(app.plugins.middleware);
+  require('cantina-redis');
+  require('cantina-session');
+  require('cantina-auth');
+  require('../');
+
+  app.on('auth:serialize', function(user) {
+    return user;
+  });
+  app.on('auth:deserialize', function(obj) {
+    return obj;
+  });
+  app.on('auth-facebook:verify', function(token, tokenSecret, profile) {
+    return profile;
+  });
+
+  app.init(function(err) {
+    if (err) return console.error(err);
+
+    app.middleware.get('/', function index(req, res) {
+      res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
+      if (req.isAuthenticated()) {
+        res.end('Welcome, <a href="https://facebook.com/' + req.user.username + '">' + req.user.displayName + '</a>!');
+      }
+      else {
+        res.end('<a href="/login">click here to login via Facebook</a>');
+      }
+    });
   });
 });
--- index.js
+++ index.js
@@ -1,32 +1,24 @@
-var FacebookStrategy = require('passport-facebook').Strategy,
+var app = require('cantina'),
+    FacebookStrategy = require('passport-facebook').Strategy,
     url = require('url');
 
-module.exports = {
-  name: 'auth-facebook',
-
-  defaults: {
+app.conf.add({
+  'auth-facebook': {
     successRedirect: '/',
-    failureRedirect: '/'
-  },
-
-  init: function(app, done) {
-    var conf = app.conf.get('auth-facebook');
-
-    if (!app.verifyFacebook) {
-      throw new Error('Your app must provide a verify callback for facebook authentication.');
-    }
-    if (!conf.callbackURL) {
-      throw new Error('You app must provide a callbackURL for facebook authentication.');
-    }
-
-    app.passport.use(new FacebookStrategy(conf, app.verifyFacebook));
-
-    if (conf.authURL) {
-      app.middleware.add(conf.authURL, app.passport.authenticate('facebook', conf));
-    }
-
-    app.middleware.add(url.parse(conf.callbackURL).path, app.passport.authenticate('facebook', conf));
-
-    done();
+    failureRedirect: '/',
+    authURL: '/login'
   }
-};
+});
+
+app.on('init', function() {
+  var conf = app.conf.get('auth-facebook');
+
+  if (!conf.callbackURL) {
+    throw new Error('You app must provide a callbackURL for facebook authentication.');
+  }
+
+  app.passport.use(new FacebookStrategy(conf, app.invoke.bind(app, 'auth-facebook:verify')));
+
+  app.middleware.add(conf.authURL, app.passport.authenticate('facebook', conf));
+  app.middleware.add(url.parse(conf.callbackURL).path, app.passport.authenticate('facebook', conf));
+});
