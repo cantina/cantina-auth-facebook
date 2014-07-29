d45527fdb039b67dba152ec675ed36343b2c285a
Brian Link <cpsubrian@gmail.com>
2014-07-28 20:00:33 -0700

    Updates to cantina 4.1.x

--- cantina-auth-facebook.js
+++ cantina-auth-facebook.js
@@ -1,26 +1,29 @@
-var app = require('cantina'),
-    FacebookStrategy = require('passport-facebook').Strategy,
-    url = require('url')
-  , conf;
+var FacebookStrategy = require('passport-facebook').Strategy,
+    url = require('url');
 
-require('cantina-web');
+module.exports = function (app) {
+  var conf;
 
-// Default conf
-app.conf.add({
-  'auth-facebook': {
-    successRedirect: '/',
-    failureRedirect: '/',
-    authURL: '/login'
+  // Dependencies.
+  app.require('cantina-web');
+
+  // Default conf
+  app.conf.add({
+    'auth-facebook': {
+      successRedirect: '/',
+      failureRedirect: '/',
+      authURL: '/login'
+    }
+  });
+
+  conf = app.conf.get('auth-facebook');
+
+  if (!conf.callbackURL) {
+    throw new Error('You app must provide a callbackURL for facebook authentication.');
   }
-});
 
-conf = app.conf.get('auth-facebook');
+  app.passport.use(new FacebookStrategy(conf, app.verifyFacebookUser));
 
-if (!conf.callbackURL) {
-  throw new Error('You app must provide a callbackURL for facebook authentication.');
-}
-
-app.passport.use(new FacebookStrategy(conf, app.verifyFacebookUser));
-
-app.middleware.add(conf.authURL, app.passport.authenticate('facebook', conf));
-app.middleware.add(url.parse(conf.callbackURL).path, app.passport.authenticate('facebook', conf));
+  app.middleware.add(conf.authURL, app.passport.authenticate('facebook', conf));
+  app.middleware.add(url.parse(conf.callbackURL).path, app.passport.authenticate('facebook', conf));
+};
